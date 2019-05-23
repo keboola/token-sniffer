9a1f5c67de8cb1b110e0e5d2511739a83cafe36d
Tomáš Fejfar <tomas.fejfar@gmail.com>
2019-05-23 16:53:25 +0200

    token sniffer

--- src/run.php
+++ src/run.php
@@ -4,19 +4,6 @@ declare(strict_types=1);
 
 use Keboola\TokenSniffer\Application;
 
-$autoloadFromProject = 'vendor/autoload.php';
-$autoloadDev = __DIR__ . '/../vendor/autoload.php';
-switch (true) {
-    case file_exists($autoloadFromProject):
-        require_once $autoloadFromProject;
-        break;
-    case file_exists($autoloadDev):
-        require_once  $autoloadDev;
-        break;
-    default:
-        throw new Exception('Missing autoload file');
-}
-
 try {
     $app = new Application();
     $app->run();
